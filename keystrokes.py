17678eadfe5cc6fb5db94b20a7c1617008799232
James Craig <jcraig@holder.com>
2022-07-01 17:22:09 -0500

    starting library

---
+++ keystrokes.py
@@ -0,0 +1,78 @@
+keystrokes = [
+    {
+        'program': 'Excel',
+        'keystrokes': [
+
+            {
+                'name':'copy',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'c']
+                ]
+            },
+            {
+                'name':'paste',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'v']
+                ]
+            },
+            {
+                'name':'copy down',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'd']
+                ]
+            },
+            {
+                'name':'copy right',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'r']
+                ]
+            },
+            {
+                'name':'underline',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'u']
+                ]
+            },
+            {
+                'name':'italics',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'i']
+                ]
+            },
+            {
+                'name':'bold',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'b']
+                ]
+            },
+            
+            
+            
+            
+            
+        ]
+    
+     },
+    {
+        'program': 'bluebeam',
+        'keystrokes': [
+
+            {
+                'name':'copy',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'c']
+                ]
+            }
+        ]
+    
+     }
+
+]
