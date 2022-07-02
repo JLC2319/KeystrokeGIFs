c6ff8ef93ae5d50476172155587019492a95be1b
James Craig <jcraig@holder.com>
2022-07-02 15:57:59 -0500

    testing

--- keystrokes.py
+++ keystrokes.py
@@ -102,6 +102,170 @@ keystrokes = [
 
         ]
     
+     },
+    {
+        'program': 'Chrome',
+        'keystrokes': [
+
+            {
+                'name':'copy',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'c']
+                ]
+            },
+            {
+                'name':'paste',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'v']
+                ]
+            },
+            {
+                'name':'New Window',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'n']
+                ]
+            },
+            {
+                'name':'New Incognito Window',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'shift'],
+                    ['ctrl', 'shift', 'n']
+                ]
+            },
+            {
+                'name':'New Tab',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 't']
+                ]
+            },
+            {
+                'name':'Reopen Closed Tabs',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'shift'],
+                    ['ctrl','shift', 't']
+                ]
+            },
+            
+            {
+                'name':'Jump to Next Tab (tab)',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'tab']
+                ]
+            },
+
+            {
+                'name':'Jump to Next Tab (pgdown)',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl', 'page down']
+                ]
+            },
+            {
+                'name':'Jump to previously open tab (tab)',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl','shift'],
+                    ['ctrl','shift', 'tab']
+                ]
+            },
+            {
+                'name':'Jump to previously open tab (pgup)',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl','page up']
+                ]
+            },
+            {
+                'name':'Open Home Page',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','home'],
+                ]
+            },
+            {
+                'name':'View previous page',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','left'],
+                ]
+            },
+            {
+                'name':'View Next page',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','right'],
+                ]
+            },
+            {
+                'name':'Close Current Tab',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl','w'],
+                ]
+            },
+            {
+                'name':'Close current window',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','f4'],
+                ]
+            },
+            {
+                'name':'Minimize Current Window',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','space'],
+                    ['n'],
+                ]
+            },
+            {
+                'name':'Maximize Current window',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','space'],
+                    ['x']
+                ]
+            },
+            {
+                'name':'Quit Chrome',
+                'keystroke': [
+                    ['alt'],
+                    ['alt','f'],
+                    ['x']
+                ]
+            },
+            {
+                'name':'Move Tab right',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl','shift'],
+                    ['ctrl','shift', 'page up'],
+                ]
+            },
+            {
+                'name':'Move Tab left',
+                'keystroke': [
+                    ['ctrl'],
+                    ['ctrl','shift'],
+                    ['ctrl','shift', 'page down'],
+                ]
+            },
+            
+            
+            
+                        
+
+
+
+        ]
+    
      }
 
 ]
